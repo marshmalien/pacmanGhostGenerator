e2c726f66dc7e535404675f36f0046b75c6bcdbe
Marliana Lara <marliana.lara@gmail.com>
2016-10-17 11:07:19 -0400

    remove empty-container on submit

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,34 @@
 (function($) {
-  /* Ghost Constructor */
-  function Ghost() {
-    console.log(this);
+  // prototype has access to context inside of constructo
+  // prototype can see inside of Constructor
+  Ghost.prototype.buildGhost = function() {
+    var source = $('#ghost').html(),
+        template = Handlebars.compile(source),
+        context = this.info,
+        html = template(context)
+
+        $(html).prependTo('.main-content').fadeIn();
   }
-  var ragnar = new Ghost();
+  /* Ghost Constructor */
+  function Ghost(context) {
+    // every ghost will have a context for it's self
+    this.info = (context);
+  }
+
+  function formValues() {
+    var context = {
+      name: $('#ghost-name').val(),
+      color: $('#ghost-color').val(),
+      primary: $('#ghost-primary').val(),
+      secondary: $('#ghost-secondary').val()
+    };
+    return context;
+  }
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    new Ghost(formValues()).buildGhost();
+    $('.empty-container').remove();
+  });
+
 })(jQuery);
